3bff5e193d35243d6d61fb6edc14901ea753c8e8
KarolisKaj <karolis.kajenas1@gmail.com>
2020-02-28 22:16:26 +0800

    In the middle of the problme 17 need to add 10 to 20 counting

--- SolvingMathProblems/Problem16.cs
+++ SolvingMathProblems/Problem16.cs
@@ -1,8 +1,9 @@
-﻿using System;
-
-namespace SolvingMathProblems
+﻿namespace SolvingMathProblems
 {
-    static internal class Problem17
+    using System;
+    using System.Linq;
+    using System.Numerics;
+    static internal class Problem16
     {
         /// <summary>
         /// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
@@ -12,97 +13,14 @@ namespace SolvingMathProblems
         {
             decimal result = 0M;
 
+            var resultInString = BigInteger.Pow(2, 1000).ToString();
+
+            for (int i = 0; i < resultInString.Length; i++)
+            {
+                result += (decimal)Char.GetNumericValue(resultInString[i]);
+            }
+
             return result;
-        }
-
-    }
-
-    public static class Problem17Helper
-    {
-        public string GetConvertedValue(decimal number)
-        {
-            var numberAsString = number.ToString();
-
-            return numberAsString.GetTextRepresentation();
-        }
-
-        public string GetTextRepresentation(this string text)
-        {
-            switch (text.Length)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-            }
-        }
-
-        public static string GetTextRepresentationOfSingleNumber(string text)
-        {
-            switch (Convert.ToInt16(text))
-            {
-                case 1:
-                    return "one";
-                case 2:
-                    return "two";
-                case 3:
-                    return "three";
-                case 4:
-                    return "four";
-                case 5:
-                    return "five";
-                case 6:
-                    return "six";
-                case 7:
-                    return "seven";
-                case 8:
-                    return "eight";
-                case 9:
-                    return "nine";
-                case 0:
-                    return String.Empty;
-                default:
-                    return String.Empty;
-            }
-        }
-
-        public static string GetTextOfTenths(string text)
-        {
-            switch (Convert.ToInt16(text))
-            {
-                case 1:
-                    return text.GetFirstTenAndTweny();
-                case 2:
-                    return "Twenty";
-                case 3:
-                    return "Thirty";
-                case 4:
-                    return "Forty";
-                case 5:
-                    return "Fifty";
-                case 6:
-                    return "Sixty";
-                case 7:
-                    return "Seventy";
-                case 8:
-                    return "Eighty";
-                case 9:
-                    return "Ninety";
-                case 0:
-                    return String.Empty;
-                default:
-                    return String.Empty;
-            }
-        }
-
-        private static string GetFirstTenAndTweny(this string text)
-        {
-            switch(text.)
-
         }
     }
 }
--- SolvingMathProblems/Problem17.cs
+++ SolvingMathProblems/Problem17.cs
@@ -1,26 +1,104 @@
-﻿namespace SolvingMathProblems
+﻿using System;
+using System.Linq;
+
+namespace SolvingMathProblems
 {
-    using System;
-    using System.Linq;
-    using System.Numerics;
-    static internal class Problem16
+    static internal class Problem17
     {
         /// <summary>
-        /// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
-        /// How many such routes are there through a 20×20 grid?
+        /// If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+        /// If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
         /// <returns></returns>
         public static decimal Answer()
         {
-            decimal result = 0M;
-
-            var resultInString = BigInteger.Pow(2, 1000).ToString();
-
-            for (int i = 0; i < resultInString.Length; i++)
+            var sut = 1000;
+            var result = 0;
+            for (int i = 1; i <= sut; i++)
             {
-                result += (decimal)Char.GetNumericValue(resultInString[i]);
+                if (i % 1000 == 0)
+                {
+                    result += "onethousand".Length;
+                }
+                if (i - 100 >= 0)
+                {
+                    result += Problem17Helper.GetTextRepresentationOfSingleNumber(i.ToString()[0]).Length + 4;
+                    if (i % 100 != 0)
+                        result += 3;
+                }
+                if ((i % 100) - 10 >= 0)
+                {
+                    result += Problem17Helper.GetTextOfTenths((i % 100).ToString()[0]).Length;
+                    if ((i % 10) != 0) result++;
+                }
+                if ((i % 10) != 0)
+                {
+                    result += Problem17Helper.GetTextRepresentationOfSingleNumber(i.ToString().Last()).Length;
+                }
             }
-
             return result;
         }
+
+    }
+
+    public static class Problem17Helper
+    {
+        public static string GetTextRepresentationOfSingleNumber(char text)
+        {
+            switch (Convert.ToInt16(text.ToString()))
+            {
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                case 0:
+                    return String.Empty;
+                default:
+                    return String.Empty;
+            }
+        }
+
+        public static string GetTextOfTenths(char text)
+        {
+            switch (Convert.ToInt16(text.ToString()))
+            {
+                case 1:
+                    return "Ten";
+                case 2:
+                    return "Twenty";
+                case 3:
+                    return "Thirty";
+                case 4:
+                    return "Forty";
+                case 5:
+                    return "Fifty";
+                case 6:
+                    return "Sixty";
+                case 7:
+                    return "Seventy";
+                case 8:
+                    return "Eighty";
+                case 9:
+                    return "Ninety";
+                case 0:
+                    return String.Empty;
+                default:
+                    return String.Empty;
+            }
+        }
+
     }
 }
